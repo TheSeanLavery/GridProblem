d054673901ce9e553b46c996fb5ed7c2d4b394e5
Sean Lavery <TheSeanLavery@gmail.com>
2020-08-04 00:56:54 -0700

    Comment Program procedure

--- GridProblem/GridProblem/Program.cs
+++ GridProblem/GridProblem/Program.cs
@@ -12,18 +12,32 @@ namespace GridProblem
     {
         static void Main(string[] args)
         {
+            //Create empty lists for rows and columns
             List<List<Vector2>> rows = new List<List<Vector2>>();
             List<List<Vector2>> columns = new List<List<Vector2>>();
 
+            //Get the filename from the arguments
             string fileName = args[0];
+
+            //Create list of points from the data file of tuples
             List<Vector2> points = FileUtils.ReadPointsFromFile(fileName);
+
+            //Calculate the square size of the grid from the points count
             int squareSize = GridUtils.GetGridSize(points.Count());
 
+            //Analyze data and extract columns
             GridUtils.CreateColumns(points, squareSize, columns);
+
+            //Use column data and extract rows
             GridUtils.CreateRows(squareSize, rows, columns);
 
+            //Write calculated rows to console
             OutputUtils.WriteRows(rows);
+
+            //Write Columns to console
             OutputUtils.WriteColumns(columns);
+
+            //Write the alpha angle to console
             OutputUtils.WriteAlpha(columns);
 
             Console.ReadLine();
