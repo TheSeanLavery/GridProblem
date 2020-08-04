3f8d40084f085ef250ecbc749e69831b9dff2907
Sean Lavery <TheSeanLavery@gmail.com>
2020-08-04 00:08:48 -0700

    extracted FindTopLeftPoint into new class

--- GridProblem/GridProblem/MathUtils.cs
+++ GridProblem/GridProblem/MathUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,26 @@ namespace GridProblem
 {
     class MathUtils
     {
-
+        public static Vector2 FindTopLeftPoint(List<Vector2> points)
+        {
+            Vector2 topLeftCommon = new Vector2(float.MaxValue, 0);
+            Vector2 topLeftPoint = new Vector2();
+            foreach (Vector2 p in points)
+            {
+                if (p.X < topLeftCommon.X) topLeftCommon.X = p.X;
+                if (p.Y > topLeftCommon.Y) topLeftCommon.Y = p.Y;
+            }
+            float distance = float.MaxValue;
+            foreach (Vector2 p in points)
+            {
+                float newDistance = Vector2.Distance(topLeftCommon, p);
+                if (newDistance < distance)
+                {
+                    topLeftPoint = p;
+                    distance = newDistance;
+                }
+            }
+            return topLeftPoint;
+        }
     }
 }
--- GridProblem/GridProblem/Program.cs
+++ GridProblem/GridProblem/Program.cs
@@ -80,7 +80,7 @@ namespace GridProblem
         static List<Vector2> GetColumn(List<Vector2> points, int count)
         {
             List<Vector2> result = new List<Vector2>();
-            Vector2 startingPoint = FindTopLeftPoint(points);
+            Vector2 startingPoint = MathUtils.FindTopLeftPoint(points);
             result.Add(startingPoint);
             points.Remove(startingPoint);
             for (int i = 0; i < count-1; i++)
@@ -92,27 +92,7 @@ namespace GridProblem
             }
             return result;
         }
-        static Vector2 FindTopLeftPoint(List<Vector2> points)
-        {
-            Vector2 topLeftCommon = new Vector2(float.MaxValue,0);
-            Vector2 topLeftPoint = new Vector2();
-            foreach(Vector2 p in points)
-            {
-                if (p.X < topLeftCommon.X) topLeftCommon.X = p.X;
-                if (p.Y > topLeftCommon.Y) topLeftCommon.Y = p.Y;
-            }
-            float distance = float.MaxValue;
-            foreach(Vector2 p in points)
-            {
-                float newDistance = Vector2.Distance(topLeftCommon, p);
-                if (newDistance < distance)
-                {
-                    topLeftPoint = p;
-                    distance = newDistance;
-                }
-            }
-            return topLeftPoint;
-        }
+        
         static Vector2 GetClosestPoint(Vector2 point, List<Vector2> points)
         {
             Vector2 closestPoint = new Vector2() ;
