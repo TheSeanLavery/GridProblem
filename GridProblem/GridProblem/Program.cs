66dbd3070d2de1a1938a62bce79f817c0726c03e
Sean Lavery <TheSeanLavery@gmail.com>
2020-08-03 17:50:37 -0700

    add FindTopLeftPoint Method

--- GridProblem/GridProblem/Program.cs
+++ GridProblem/GridProblem/Program.cs
@@ -30,6 +30,8 @@ namespace GridProblem
                 _ = GetClosestPoint(p, points);
             }
 
+            Vector2 topLeftPoint = FindTopLeftPoint(points);
+
             //This should work as 9 is a square number
             //int testSquareSize = GetGridSize(9);
 
@@ -38,7 +40,29 @@ namespace GridProblem
 
             Console.ReadLine();
         }
+        static Vector2 FindTopLeftPoint(List<Vector2> points)
+        {
+            Vector2 topLeftCommon = new Vector2(float.MaxValue,0);
+            Vector2 topLeftPoint = new Vector2();
 
+            foreach(Vector2 p in points)
+            {
+                if (p.X < topLeftCommon.X) topLeftCommon.X = p.X;
+                if (p.Y > topLeftCommon.Y) topLeftCommon.Y = p.Y;
+            }
+            float distance = float.MaxValue;
+            foreach(Vector2 p in points)
+            {
+                float newDistance = Vector2.Distance(topLeftCommon, p);
+                if (newDistance < distance)
+                {
+                    topLeftPoint = p;
+                    distance = newDistance;
+                }
+            }
+            return topLeftPoint;
+
+        }
         static Vector2 GetClosestPoint(Vector2 point, List<Vector2> points)
         {
             Vector2 closestPoint = new Vector2() ;
