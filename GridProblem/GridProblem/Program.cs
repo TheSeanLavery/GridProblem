e0d5e0f5f2143740f0e766b8247bb6448caa46c4
Sean Lavery <TheSeanLavery@gmail.com>
2020-08-04 15:46:46 -0700

    Set angleMin to be positive

--- GridProblem/GridProblem/Program.cs
+++ GridProblem/GridProblem/Program.cs
@@ -133,7 +133,7 @@ namespace GridProblem
         public int seed = 1337;
         public int coordMin = -50;
         public int coordMax = 50;
-        public int angleMin = -45;
+        public int angleMin = 0;
         public int angleMax = 45;
     }
 }
